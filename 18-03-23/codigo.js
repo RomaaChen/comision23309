22582ccd8b820c31f475907dd4ff65e2416d1b21
Nicolás Farji <romana.chen@gmail.com>
2023-03-19 16:47:02 -0300

    se agrega material teorico

---
+++ 18-03-23/codigo.js
@@ -0,0 +1,76 @@
+// //interacciones
+
+// //alerta
+
+// alert ("Hola, esta es mi pagina");
+
+// //prompt
+
+// let nombre = prompt("Decime tu nombre");
+
+// // confirm
+
+// confirm ( nombre + ", queres ingresar?"); //es un buriano
+
+// // variables
+
+// //let var const
+
+// // tipos de datos
+
+// let nombre2 = "Romana" // cadena de texto = string
+
+// nombre2 = 32 // numero = numbre
+
+// nombre2 = false // booleano = boolean (true-false)
+
+// nombre2 = ["Superman" , "Batman"] // Array
+
+// let nombreApellido = "Juan" //diferencia mayúsculas de minúsculas
+
+// //document.write(nombre2)
+
+// console.log(nombre2)
+
+
+// let numero1 = 20
+
+// let numero2 = Number("20") // puede ser parseInt o parseNumer
+
+// //let resultado = numero1*numero2
+
+// document.write(resultado)
+
+// let nombre3 = prompt("Decime tu nombre")
+// let num1 = Number(prompt("Ingresa un número para sumar"))
+// let num2 = Number(prompt("Ingresa otro número para sumar"))
+// let resultado = num1 + num2
+
+// alert ( nombre3 + " el resultado es : " + resultado)
+
+// constantes 
+// const heroe = "batman";
+
+// heroe = "Superman"
+
+// console.log(heroe)
+
+// operadores +, -, /, *, %
+
+/* let num1 = 23
+let num2 = 7
+
+document.write(num1%num2) // da el resto
+ */
+//Math
+
+// document.write(Math.pow(3,3)) // numero y exponente
+
+
+// document.write(Math.round(6.5)) //redondea al numero entero mas cercano
+
+//document.write(Math.round(6.45)) //redondea al numero entero mas cercano
+
+//document.write(Math.floor(6.9999)) //redondea al numero entero mas bajo
+
+document.write(Math.ceil(6.0001)) // redondeo al numero entero mas arriba
